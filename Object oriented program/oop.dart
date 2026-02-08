39d4f09202732195e1b1f8a6c99db85e56c6bdb0
Sharif35-958 <ssharif222.ss@gmail.com>
2026-02-09 02:23:21 +0600

    OOP

--- Object oriented program/oop.dart
+++ Object oriented program/oop.dart
@@ -1,3 +1,18 @@
 main() {
-  
+    PersonClass person = PersonClass();
+ person.one();
+ person.two();
+ print(person.name);
 }
+ class PersonClass{
+    var name= 'John';
+    var age= 20;
+    var gender= 'male';
+
+    one(){
+        print('Hello, my name is $name, I am $age years old and I am a $gender.');
+    }
+    two(){
+        print('I am a student and I love learning new things.');
+    }
+  }
